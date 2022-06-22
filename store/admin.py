a065c079ff6b11b237889b55f44fdeacb50ae15a
s13g <ayflix0@gmail.com>
2022-06-22 14:03:23 +0100

    prduct images

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from store.models import Product, Category
+from store.models import Product, Category, ProductImage
 
-admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+
--- store/models.py
+++ store/models.py
@@ -1,25 +1,17 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 
-
 # Create your models here.
+from store.validators import validate_file_size
 
 
 class Category(models.Model):
-    Male = 'M'
-    Female = 'F'
-
     Sale_sport = 'S'
     Sale_luxury = 'L'
 
     Bag_product = 'B'
     Sweather_product = 'S'
     Sunglass_product = 'SG'
-
-    GENDER_CHOICES = [
-        (Male, 'Male'),
-        (Female, 'Female'),
-    ]
 
     SALE_CHOICES = [
         (Sale_sport, 'Sports'),
@@ -33,7 +25,6 @@ class Category(models.Model):
     ]
 
     title = models.CharField(max_length=255)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='', blank=True)
     sale = models.CharField(max_length=5, choices=SALE_CHOICES, default='', blank=True)
     product = models.CharField(max_length=5, choices=PRODUCT_CHOICES, default='', blank=True)
 
@@ -42,15 +33,31 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    All = 'A'
+    Male = 'M'
+    Female = 'F'
+
+    GENDER_CHOICES = [
+        (All, 'All'),
+        (Male, 'Male'),
+        (Female, 'Female'),
+    ]
+
     title = models.CharField(max_length=255)
     brand = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     featured_product = models.BooleanField(default=False, null=True)
     specifications = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=False, related_name='category')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='', blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.IntegerField(validators=[MinValueValidator(1)])
     last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='store/images', validators=[validate_file_size])
