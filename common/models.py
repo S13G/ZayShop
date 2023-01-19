e5da728d3c049fee4182f16182b8ea7fb4fd4672
s13g <ayflix0@gmail.com>
2023-01-19 22:59:12 +0100

    refactored store models

--- common/models.py
+++ common/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 
 class TimeStampedUUID(models.Model):
-    id = models.UUIDField(default=uuid4(), unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid4, unique=True, primary_key=True, editable=False)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
--- store/models.py
+++ store/models.py
@@ -10,7 +10,7 @@ from common.models import TimeStampedUUID
 
 class Category(TimeStampedUUID):
     name = models.CharField(max_length=255, null=True)
-    slug = AutoSlugField(populate_from=name, unique=True, always_update=True, null=True)
+    slug = AutoSlugField(populate_from="name", unique=True, always_update=True, null=True)
 
     class Meta:
         ordering = ["-created"]
@@ -21,14 +21,9 @@ class Category(TimeStampedUUID):
 
 
 class SubCategory(TimeStampedUUID):
-    GENDER_CHOICES = (
-        ("M", "MALE"),
-        ("F", "FEMALE")
-    )
     name = models.CharField(max_length=255, null=True)
-    slug = AutoSlugField(populate_from=name, unique=True, always_update=True, null=True)
+    slug = AutoSlugField(populate_from="name", unique=True, always_update=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name="sub_category")
-    gender = models.CharField(max_length=255, choices=GENDER_CHOICES, default="M", null=True)
 
     class Meta:
         ordering = ["-created"]
@@ -40,6 +35,12 @@ class SubCategory(TimeStampedUUID):
 
 
 class Product(TimeStampedUUID):
+    GENDER_CHOICES = (
+        ("A", "ALL"),
+        ("M", "MALE"),
+        ("F", "FEMALE"),
+    )
+
     SIZE_CHOICES = (
         ('S', 'Small'),
         ('M', 'Medium'),
@@ -47,7 +48,7 @@ class Product(TimeStampedUUID):
         ('XL', 'Extra Large')
     )
     name = models.CharField(max_length=255, null=True)
-    slug = AutoSlugField(populate_from=name, unique=True, always_update=True, null=True)
+    slug = AutoSlugField(populate_from="name", unique=True, always_update=True, null=True)
     category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, related_name="products")
     product_main_image = models.ImageField(default="assets/img/default.jpg", upload_to="media", null=True)
     second_product_image = models.ImageField(default="assets/img/default.jpg", upload_to="media", null=True, blank=True)
@@ -58,9 +59,11 @@ class Product(TimeStampedUUID):
     available_color = models.CharField(max_length=400, null=True, blank=True)
     specifications = models.TextField(null=True, blank=True)
     brand = models.CharField(max_length=255, null=True, blank=True)
+    gender = models.CharField(max_length=255, choices=GENDER_CHOICES, default="M", null=True)
     sizes = models.CharField(max_length=255, choices=SIZE_CHOICES, default=None, null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=10, validators=[MinValueValidator(0)], null=True)
-    quantity = models.PositiveIntegerField(default=0, null=True)
+    available_quantity = models.PositiveIntegerField(default=0, null=True)
+    quantity = models.PositiveIntegerField(default=None, null=True, blank=True)
     featured = models.BooleanField(default=False, null=True)
 
     class Meta:
